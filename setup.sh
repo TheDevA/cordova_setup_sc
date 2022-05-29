e757c43afb4a813a92965b07360c6c99dc680bdb
Zer0_Day <54479109+TheDevA@users.noreply.github.com>
2022-05-29 19:38:00 +0100

    Create setup.sh
    ed n01

---
+++ setup.sh
@@ -0,0 +1,27 @@
+echo "starting.."
+
+echo "installing packges.."
+apt -y install nodejs npm openjdk-8-jdk
+
+echo "installing cordova..."
+npm install -g cordova
+
+echo "makeing dirs.." 
+cd $HOME/Desktop/
+
+mkdir test
+
+mkdir android
+
+echo "creating cordova project..."
+cordova create hello com.example.hello HelloWorld
+
+cd $HOME/Desktop/test/hello
+
+echo "adding android platform to the project"
+cordova platform add android
+
+echo "cordova requirements:"
+cordova requirements
+
+
